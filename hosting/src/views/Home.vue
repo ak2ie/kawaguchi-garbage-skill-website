a9a4e60628176004175c37894dfd6a7d8d36218d
ak2ie <mail@ak2ie.net>
2021-02-02 23:02:17 +0900

    トップページを修正

--- hosting/src/views/Home.vue
+++ hosting/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <p>Alexaスキル「川口のゴミ分別」の地域を登録します。</p>
-    <router-link to="/login">ログイン</router-link>
-  </div>
+  <v-main>
+    <v-container>
+      <v-row>
+        <v-col>
+          <div class="home">
+            <p>
+              お住まいの地域を登録すると、Alexaスキル「川口のゴミ分別」で次のゴミ出しの日が分かるようになります。
+            </p>
+            <router-link to="/login">ログイン</router-link>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component({})
 export default class Home extends Vue {}
 </script>
